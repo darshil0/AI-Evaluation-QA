97e068685ad4bc3b06330f5ec1597028609e8689
Darshil Shah <darshils99@gmail.com>
2026-06-28 18:04:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ except ImportError as e:
 
 
 @click.group()
-@click.version_option(version="2.5.0")
+@click.version_option(version="2.5.1")
 def cli() -> None:
     """AI Evaluation QA Framework CLI."""
 
